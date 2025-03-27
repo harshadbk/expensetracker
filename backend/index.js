1ee1e7172c3145cac988f69fbb8fd3edf79e3ff3
harshadbk <khataleharshad26@gmail.com>
2025-03-27 16:22:27 +0530

    done my backend

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,15 @@ const cors = require('cors');
 require('dotenv').config();
 const jwt = require("jsonwebtoken");
 
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
 
 mongoose
